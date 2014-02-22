121b8311043af7517c3f159ebe77f1f61bcdb7f1
Rosie Hoyem <rosiehoyem@gmail.com>
2014-02-22 12:00:35 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CivicSocialMediaFinder::Application.config.secret_key_base = '584c4a020e134ab91a5926deb70a7fd4e0d854352de8dd6a709515eb794a67bcdee569aca1e8da8e801f1f01f7580d77d1e4fd494b4ae34d2bdff7a7994f42e1'
